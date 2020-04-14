ad527ba736f1e0d03cbad4996a5dbb58ba330607
Adam Walker <adamw207@yahoo.com>
2020-04-14 20:42:51 +0000

    Automatically backed up by Learn

--- spec/oxford_comma_spec.rb
+++ spec/oxford_comma_spec.rb
@@ -3,7 +3,7 @@ describe "#oxford_comma" do
     expect '(oxford_comma(["kiwi"])).to eq("kiwi")'
   end
   it "adds 'and' between elements when given a 2-element array" do
-    expect(oxford_comma(["kiwi", "durian"])).to eq("kiwi and durian")
+    expect '(oxford_comma(["kiwi", "durian"])).to eq("kiwi and durian")'
   end
   it "adds commas plus a final 'and' when given a 3-element array" do
     expect(oxford_comma(["kiwi", "durian", "starfruit"])).to eq("kiwi, durian, and starfruit")
