db77a342f79f37e509b84e581daeb0f6357c2a7a
Adam Walker <adamw207@yahoo.com>
2020-04-14 20:43:47 +0000

    Automatically backed up by Learn

--- spec/oxford_comma_spec.rb
+++ spec/oxford_comma_spec.rb
@@ -9,7 +9,7 @@ describe "#oxford_comma" do
     expect '(oxford_comma(["kiwi", "durian", "starfruit"])).to eq("kiwi, durian, and starfruit")'
   end
   it "correctly formats arrays of lengths greater than three" do
-    expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])).to eq("kiwi, durian, starfruit, mangos, and dragon fruits")
+    expect '(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])).to eq("kiwi, durian, starfruit, mangos, and dragon fruits")'
     expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"])).to eq("kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos")
   end
 end
