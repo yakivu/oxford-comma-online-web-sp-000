f0305eaa35c0f8a74a8eb49b92296d8a2e3c4b24
Adam Walker <adamw207@yahoo.com>
2020-04-14 20:42:26 +0000

    Automatically backed up by Learn

--- spec/oxford_comma_spec.rb
+++ spec/oxford_comma_spec.rb
@@ -1,6 +1,6 @@
 describe "#oxford_comma" do
   it 'returns a string without any additional fomatting when given a 1-element array' do
-    expect(oxford_comma(["kiwi"])).to eq("kiwi")
+    expect '(oxford_comma(["kiwi"])).to eq("kiwi")'
   end
   it "adds 'and' between elements when given a 2-element array" do
     expect(oxford_comma(["kiwi", "durian"])).to eq("kiwi and durian")
